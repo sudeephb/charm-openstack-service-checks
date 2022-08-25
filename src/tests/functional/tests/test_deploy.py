933884a3f3f7f8221426c3f39160370c10860fe6
jneo8 <james.lin@canonical.com>
2022-08-25 03:35:24 +0000

    test(src/tests): Fix functional test
    * Remove latest/edge channel on focal bundle
    * Adjust sleep time on functional test and add comment
    * Change workload-status-msg to fit the difference msg on different
    series.

--- src/tests/functional/tests/test_deploy.py
+++ src/tests/functional/tests/test_deploy.py
@@ -212,7 +212,10 @@ class TestOpenStackServiceChecks(TestBase):
 
         # test valid configuration
         model.set_application_config(self.application_name, {"check-servers": "1,2"})
-        time.sleep(60)
+
+        # model.block_until_all_units_idle fire too quick before config change
+        # So we sleep a while wait for application react.
+        time.sleep(10)
         model.block_until_all_units_idle()
 
         result = model.run_on_unit(self.lead_unit_name, cmd)
